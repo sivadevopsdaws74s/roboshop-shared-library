613bc45fb6861ffe60668a0a2603c90d2f5d4123
sivakumarreddy <info@joindevops.com>
2023-11-23 08:23:13 +0530

    added libraries

--- vars/nodeJSEKS.groovy
+++ vars/nodeJSEKS.groovy
@@ -94,7 +94,7 @@ def call(Map configMap){
                         sh """
                             cd helm
                             sed -i 's/IMAGE_VERSION/$packageVersion/g' values.yaml
-                            helm install ${component} .
+                            helm upgrade ${component} .
                         """
                     }
                 }
